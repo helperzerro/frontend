71cd1aade0e1040bec52cfa0443f08e3be27d606
helperzerro <wilmars2710@gmail.com>
2022-08-14 21:02:52 +0700

    ubah router agar ke Page slider

--- src/config/Router/index.jsx
+++ src/config/Router/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Homez, Contact, About, Slider } from "../../pages";
+// import { Homez, Contact, About, Slider } from "../../pages";
+import { Slider } from "../../pages";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homez />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Slider />} />
+        {/* <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-        <Route path="/slider" element={<Slider />} />
+        <Route path="/slider" element={<Slider />} /> */}
       </Routes>
     </BrowserRouter>
   );
